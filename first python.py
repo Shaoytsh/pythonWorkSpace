19279acf548397b537ad277af84283d47ba9a65c
Shaoytsh-PC <shyttsyj@126.com>
2018-02-06 16:30:05 +0800

    1

---
+++ first python.py
@@ -0,0 +1,3 @@
+__author__ = 'Shaoytsh-PC'
+#-*- coding: utf-8 -*
+print("hello word!\n first python ! \r\r you can do it!")
---
+++ second python.py
@@ -0,0 +1,14 @@
+__author__ = 'Shaoytsh-PC'
+#-*- coding: utf-8 -*
+word = 'Python'
+
+print(word[4:]+word[:4]+word[2:5])
+s = 'supercalifragilisticexpialidocious'
+print(len(s))
+squares = [1, 4, 9, 16, 25]
+print(squares[-2:])
+
+a, b = 0, 1
+while b<10:
+    print(b,end=',')
+    a, b = b, a+b
