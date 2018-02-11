2f60546c1165e935dbedcaa525e4b006abe037e6
Shaoytsh-PC <shyttsyj@126.com>
2018-02-11 12:45:13 +0800

    1

--- first python.py
+++ first python.py
@@ -1,3 +1,7 @@
 __author__ = 'Shaoytsh-PC'
 #-*- coding: utf-8 -*
 print("hello word!\n first python ! \r\r you can do it!")
+#明确的行连接
+s = 'This is a string. \
+This continues the string.'
+print (s)
